71d1478d25dc91bc64759dc0e69831174604118f
wangwenwewn <wangww@resthour.net>
2024-03-14 13:51:34 +0800

    init dayutils

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+const babel = require('rollup-plugin-babel')
+const { terser } = require('rollup-plugin-terser')
+
+const path = require('path')
+const nodeResolve = require('@rollup/plugin-node-resolve')
+
+const resolve = function (...args) {
+  return path.resolve(__dirname, ...args)
+}
+
+const extensions = ['.js', '.ts']
+module.exports = {
+  input: {
+    index: resolve('./src/index.ts')
+  },
+  output: [
+    {
+      dir: 'dist/umd',
+      name: 'dayUtils',
+      format: 'umd',
+      globals: {
+        dayjs: 'dayUtils'
+      }
+    },
+    {
+      dir: 'dist/cjs',
+      format: 'cjs'
+    },
+    {
+      dir: 'dist/es',
+      format: 'es'
+    }
+  ],
+  plugins: [
+    nodeResolve({
+      extensions,
+      modulesOnly: true
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      extensions
+    }),
+    terser()
+  ]
+}
